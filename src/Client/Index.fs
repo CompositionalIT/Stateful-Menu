2e180039298e5f96055f6a8a007346055ddb6293
Ryan Palmer <ryan@compositional-it.com>
2021-01-19 18:53:35 +0000

    formatting

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -85,32 +85,33 @@ let containerBox (model : Model) (dispatch : Msg -> unit) =
         ]
     ]
     
-let view (model : Model) (dispatch : Msg -> unit) = Menu.view [
-    Hero.hero [
-        Hero.Color IsPrimary
-        Hero.IsFullHeight
-        Hero.Props [
-            Style [
-                Background """linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://unsplash.it/1200/900?random") no-repeat center center fixed"""
-                BackgroundSize "cover"
+let view (model : Model) (dispatch : Msg -> unit) =
+    Menu.view [
+        Hero.hero [
+            Hero.Color IsPrimary
+            Hero.IsFullHeight
+            Hero.Props [
+                Style [
+                    Background """linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://unsplash.it/1200/900?random") no-repeat center center fixed"""
+                    BackgroundSize "cover"
+                ]
             ]
-        ]
-    ] [
-        Hero.head [ ] [
-            Navbar.navbar [ ] [
-                Container.container [ ] [ navBrand ]
+        ] [
+            Hero.head [ ] [
+                Navbar.navbar [ ] [
+                    Container.container [ ] [ navBrand ]
+                ]
             ]
-        ]
-        Hero.body [ ] [
-            Container.container [ ] [
-                Column.column [
-                    Column.Width (Screen.All, Column.Is6)
-                    Column.Offset (Screen.All, Column.Is3)
-                ] [
-                    Heading.p [ Heading.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ] [ str "Stateful_Menu" ]
-                    containerBox model dispatch
+            Hero.body [ ] [
+                Container.container [ ] [
+                    Column.column [
+                        Column.Width (Screen.All, Column.Is6)
+                        Column.Offset (Screen.All, Column.Is3)
+                    ] [
+                        Heading.p [ Heading.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ] [ str "Stateful_Menu" ]
+                        containerBox model dispatch
+                    ]
                 ]
             ]
         ]
     ]
-]
